85c0efa435c53782255c2d0d1b3ea5af2e890682
jtang93 <tang.jonathan93@gmail.com>
2018-10-17 21:11:01 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,22 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: {in: %w("Fiction", "Non-fiction")}
+  validates :category, inclusion: {in: ["Fiction", "Non-fiction"]}
+  validate :clickbait
+
+  def clickbait
+    array = ["Won't Believe", "Secret", "Top", "Guess"]
+    # binding.pry
+    if title != nil
+      if array.any? {|bait| title.include? bait} == false
+        errors.add :title, "clickbait"
+      # unless record.name & array.present?
+      #   post.errors[:name] << "Need a name containing #{array}."
+      end
+    end
+  end
 
 end
